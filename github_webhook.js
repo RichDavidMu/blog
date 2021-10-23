d747ea3315ffedf8aa96dea0e69231d8b8b61728
JohnnyMu <404345032@qq.com>
2021-10-24 01:02:45 +0800

    init webhook

---
+++ github_webhook.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const app = new Koa();
+const child_process = require('child_process')
+
+const main = ctx => {
+    console.log(ctx.request.headers)
+    console.log(ctx.request.body)
+    const cmd = 'git pull'
+    const process = child_process.spawn(cmd, {shell: true})
+    process.stderr.on('data', (data) => {
+      console.error(data.toString())
+      const response = {code: 500, message: 'update failed'}
+      ctx.response.body = response
+      ctx.status = 500
+    })
+    process.stdout.on('data',(data)=>{
+      console.log(data.toString())
+      const response = {code: 200, message: 'update successfully'}
+      ctx.response.body = response
+      ctx.status = 200
+  })
+  };
+  
+app.use(main);
+app.listen(3000);
+console.log('server listen at port 3000')
---
+++ pm2.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "github_webhook",
+    script : "./github_webhook.js",
+    watch: true,
+    log_date_format: "YYYY-MM-DD HH:mm Z",
+    combine_logs: true,
+    merge_logs: true,
+  }]
+}
