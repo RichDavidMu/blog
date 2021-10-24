afc83506007ef1d725cc0a183b7c077f49ab8a15
JohnnyMu <404345032@qq.com>
2021-10-25 00:28:57 +0800

    fix bug

--- github_webhook.js
+++ github_webhook.js
@@ -6,7 +6,8 @@ const main = ctx => {
     console.log(ctx.request.headers)
     console.log(ctx.request.body)
     console.log('start cmd')
-    const cmd = 'git stash && git pull && hexo clean && hexo generate && pm2 start hexo_server.js'
+    const cmd = 'git stash && git pull'
+    console.log(cmd)
     const process = child_process.spawn(cmd, {shell: true})
     process.stderr.on('data', (data) => {
       console.error(data.toString())
@@ -14,11 +15,28 @@ const main = ctx => {
       ctx.response.body = response
       ctx.status = 500
     })
-    process.stdout.on('data',(data)=>{
-      console.log(data.toString())
-      const response = {code: 200, message: 'update successfully'}
-      ctx.response.body = response
-      ctx.status = 200
+    process.stdout.on('close',()=>{
+      const cmd1 = 'hexo clean'
+      console.log(cmd1)
+      const process1 = child_process.spawn(cmd1, {shell: true})
+      process1.stdout.on('close',()=>{
+        const cmd2 = 'hexo generate'
+        console.log(cmd2)
+        const process2 = child_process.spawn(cmd2, {shell: true})
+        process2.stdout.on('close',()=>{
+          const cmd3 = 'pm2 start hexo_server.js'
+          console.log(cmd3)
+          const process3 = child_process.spawn(cmd3, {shell: true})
+          process3.on('close',()=>{
+            console.log(data.toString())
+            const response = {code: 200, message: 'update successfully'}
+            ctx.response.body = response
+            ctx.status = 200
+          })
+        })
+      
+      })
+      
   })
   };
   
