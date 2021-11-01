2eb5f54b3ba47a826f1791e5e3f7c3541299a89e
JohnnyMu <404345032@qq.com>
2021-11-01 21:53:13 +0800

    add https

--- hexo_server.js
+++ hexo_server.js
@@ -2,6 +2,13 @@ const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
  
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const enforceHttps = require('koa-sslify').default
+
+app.use(enforceHttps())
+
 // $ GET /package.json
 // app.use(serve('.'));
  
@@ -11,6 +18,13 @@ app.use(serve('public'));
 // or use absolute paths
 // app.use(serve(__dirname + '/public'));
  
-app.listen(80);
+// app.listen(80);
  
-console.log('listening on port 80');
+const options = {
+	key: fs.readFileSync('./ssl/luoluoqinghuan.cn.key'),
+	cert: fs.readFileSync('./ssl/luoluoqinghuan.cn.pem')
+}
+
+http.createServer(app.callback()).listen(80);
+https.createServer(options, app.callback()).listen(443)
+console.log('listening on port 80 443');
