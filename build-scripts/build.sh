9249b245049452798010ada8b23befa0d7faf178
muchenwei <muchenwei@baidu.com>
2026-01-05 21:37:31 +0800

    博客源文件也扔到服务器里

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 pnpm run build
-mkdir -p public/resources
-cp -r source/_posts public/resources/_posts
-cp -r source/about public/resources/about
+mkdir -p public/source
+cp -r source public/source
